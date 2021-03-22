2523719e58c3312c77ac636861db1928ed4c4f52
syahidabdur7 <58288963+syahidabdur7@users.noreply.github.com>
2021-03-22 11:05:40 +0700

    Add files via upload

---
+++ CountLetters.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package W2;
+
+/**
+ *
+ * @author User
+ */
+import java.util.Scanner;
+public class CountLetters {
+    public static void main(String[] args)
+ {
+ int[] counts = new int[26];
+ Scanner scan = new Scanner(System.in);
+ //get word from user
+ System.out.print("Enter a single word (letters only, please): ");
+ String word = scan.nextLine();
+ //convert to all upper case
+ word = word.toUpperCase();
+ //count frequency of each letter in string
+ try{
+ for (int i=0; i < word.length(); i++)
+ counts[word.charAt(i)-'A']++;
+ //print frequencies
+ System.out.println();
+ for (int i=0; i < counts.length; i++)
+ if (counts [i] != 0)
+ System.out.println((char)(i +'A') + ": " + counts[i]);
+ }catch (Exception e){
+  System.out.println("Not a Letter"); 
+   }
+ }
+}
